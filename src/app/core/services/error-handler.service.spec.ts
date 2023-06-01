ff58606cc867f057e949a7813d9ef1c28488e5c4
Moein Hosseini <rxmoein@tuta.io>
2023-06-01 18:10:11 +0200

    Fix unit tests

--- src/app/core/services/error-handler.service.spec.ts
+++ src/app/core/services/error-handler.service.spec.ts
@@ -1,12 +1,21 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { TestBed } from '@angular/core/testing';
 
 import { CustomErrorHandler } from './error-handler.service';
 
 describe('CustomErrorHandlerService', () => {
   let service: CustomErrorHandler;
+  let snackBarMock = {
+    open: jest.fn(),
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{
+        provide: MatSnackBar,
+        useValue: snackBarMock,
+      }]
+    });
     service = TestBed.inject(CustomErrorHandler);
   });
 
--- src/app/core/services/snackbar.service.spec.ts
+++ src/app/core/services/snackbar.service.spec.ts
@@ -1,7 +1,7 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { TestBed } from '@angular/core/testing';
 
 import { SnackbarService } from './snackbar.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 describe('SnackbarService', () => {
   let service: SnackbarService;
