0cf6c5682e022f227796c748ea5b9906fc2e44d4
Moein Hosseini <rxmoein@tuta.io>
2023-06-04 01:15:11 +0200

    Fix unit test bug

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
-import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
+    imports: [RouterTestingModule, CoreModule, SharedModule],
     declarations: [AppComponent]
   }));
 
