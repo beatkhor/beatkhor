6dbb2695286bce416cd1d3cf2c37787997ed28d3
Moein Hosseini <rxmoein@tuta.io>
2023-06-08 16:57:56 +0200

    Update production environments

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -3,8 +3,8 @@ import { messages } from './messages';
 export const environment = {
     production: true,
     storageKeyPrefix: 'bk',
-    authServiceURL: 'https://beatkhor.com/api/auth',
-    contentServiceURL: 'https://beatkhor.com/api/content',
-    storageServiceURL: 'https://beatkhor.com/api/storage',
+    authServiceURL: 'https://prod.beatkhor.com/api/auth',
+    contentServiceURL: 'https://prod.beatkhor.com/api/content',
+    storageServiceURL: 'https://prod.beatkhor.com/api/storage',
     messages,
 };
